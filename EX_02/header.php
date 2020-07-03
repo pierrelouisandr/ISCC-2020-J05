46577ed9dda2fe7b33f9838eebf0df1027ea2e0f
pierrelouisandr <67279555+pierrelouisandr@users.noreply.github.com>
2020-07-03 16:05:22 +0200

    Jour N°5

--- EX_02/header.php
+++ EX_02/header.php
@@ -1,5 +1,5 @@
 <header>
-<?php include("navigation.php"); ?>
+<?php include ("navigation.php"); ?>
     
     <h1> Summer Code Camp </h1>
     </header>
--- EX_02/navigation.php
+++ EX_02/navigation.php
@@ -1,7 +1,12 @@
+
 <nav>
+
+
  
 <a <?php if ($page_title == "Accueil") {echo 'class="active"';} ?> href="vitrine-accueil.php"> ACCUEIL </a>
 <a <?php if ($page_title == "Programme") {echo 'class="active"';} ?> href="vitrine-programme.php"> PROGRAMME </a>
 <a <?php if ($page_title == "Contact") {echo 'class="active"';} ?> href="vitrine-contacts.php"> CONTACTS </a>
 
 </nav>
+
+    
